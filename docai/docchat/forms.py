92a2d541bee9f574f05fc22dcbffd2546f903b95
Justine-Paulraj <124027839+Justine-Paulraj@users.noreply.github.com>
2025-09-16 01:25:58 +0530

    Delete docai directory

--- docai/docchat/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-class DocumentUploadForm(forms.Form):
-    file = forms.FileField(label="Upload a PDF")
-
-class QuestionForm(forms.Form):
-    question = forms.CharField(label="Ask a question", max_length=500, widget=forms.TextInput(attrs={'size': '60'}))
